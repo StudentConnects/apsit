035c80024a9bdc4e0267ba09cc7adaf7d7b45909
Devesh Yadav <darkkd11@gmail.com>
2020-09-25 12:00:43 +0530

    Registration form changed

--- public/assets/js/register.js
+++ public/assets/js/register.js
@@ -4,14 +4,13 @@ function signup() {
     let fullname = document.getElementById("fullname").value;
     let email = document.getElementById("email").value;
     let mobile = document.getElementById("mobile").value;
-    let institute = document.getElementById("institute").value;
     let password = document.getElementById("password").value;
 
-    if (!(isFormEmpty(fullname, email, mobile, institute, password))) {
+    if (!(isFormEmpty(fullname, email, mobile,  password))) {
         if (checkName(fullname)) {
             if (validateEmail(email)) {
                 if (checkNumber(mobile)) {
-                    submit_details(fullname, email, mobile, institute, password);
+                    // submit_details(fullname, email, mobile,  password);
                 }
             }
 
@@ -20,14 +19,14 @@ function signup() {
     }
 }
 
-function isFormEmpty(fullname, email, mobile, institute, password) {
+function isFormEmpty(fullname, email, mobile,  password) {
 
-    if (fullname.length == 0 || email.length == 0 || mobile.length == 0 || institute.length == 0, password.length == 0) {
+    if (fullname.length == 0 || email.length == 0 || mobile.length == 0 ||  password.length == 0) {
         alert("Fields can't be left empty");
         return true;
     }
     else {
-        if (email.length > 60 || institute.length > 60 || password.length > 1024) {
+        if (email.length > 60 ||  password.length > 1024) {
             alert("Field Value Too Long");
             return true;
         }
@@ -79,21 +78,7 @@ function checkName(fullname) {
 
 
 
-function submit_details(fullname, email, mobile, institute, password) {
-
-}
-
-var reader = new FileReader();
-reader.onload = function (e) {
-    $('#profile_avatar').attr('src', e.target.result);
-}
-   
-   function readURL(input) {
-        if (input.files && input.files[0]) {
-            reader.readAsDataURL(input.files[0]);
-        }
-    }
+function submit_details(fullname, email, mobile,  password) {
     
-    $("#profile_pic").change(function(){
-        readURL(this);
-    });
+}
+
